26fe31daaf78434fbc5b69cef3c97e29c4608289
Yangyangzhong82 <utorabrl1wkmvg@gmail.com>
2025-05-06 00:00:34 +0800

    xmake

--- xmake.lua
+++ xmake.lua
@@ -13,6 +13,8 @@ end
 add_requires("levibuildscript")
 add_requires("sqlite3")
 add_requires("mysql")
+add_requires("legacyremotecall")
+
 if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
@@ -29,7 +31,7 @@ target("Bedrock-Authority") -- Change this to your mod name.
     add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
     -- Add BA_EXPORTS to enable dllexport/dllimport macros
     add_defines("NOMINMAX", "UNICODE", "BA_EXPORTS")
-    add_packages("levilamina","sqlite3","mysql")
+    add_packages("levilamina","sqlite3","mysql" ,"legacyremotecall")
     set_exceptions("none") -- To avoid conflicts with /EHa.
     set_kind("shared")
     set_languages("c++20")
@@ -44,3 +46,12 @@ target("Bedrock-Authority") -- Change this to your mod name.
     --     add_includedirs("src-client")
     --     add_files("src-client/**.cpp")
     -- end
+        after_build(function (target)
+        local bindir = path.join(os.projectdir(), "bin")
+        local includedir = path.join(bindir, "include", "Bedrock-Authority") -- 修改目标包含目录
+        local libdir = path.join(bindir, "lib")
+        os.mkdir(includedir)
+        os.mkdir(libdir)
+        os.cp(path.join(os.projectdir(), "src", "permission", "**.h"), includedir) -- 只复制czmoney子目录下的头文件
+        os.cp(path.join(target:targetdir(), target:name() .. ".lib"), libdir)
+        end)
